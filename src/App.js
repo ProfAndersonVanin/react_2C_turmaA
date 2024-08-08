f30de76a271ae8f2fdc84af4960c3971053043a8
profandersonvanin <profandersonvanin@gmail.com>
2024-08-08 10:57:38 -0300

    Aula do dia 08-08-24

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import Banner from "./components/Banner";
+import Container from "./components/Container";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <Header/>
-      <h1>ETEC MCM</h1>
-      <p>Olá Mundo!</p>
+      <Banner/>
+      <Container>
+        <h1>ETEC MCM NOVOOOOOOOO</h1>
+        <p>Olá Mundo!</p>
+      </Container>      
+      <Footer/>
     </>
   );
 }
